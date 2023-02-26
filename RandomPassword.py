e8e95ce9b511ab1f354e4c00238ada22be3ccd12
Leo Wang <77943019+Le0wang06@users.noreply.github.com>
2023-02-26 20:11:23 +0000

    update

--- RandomPassword.py
+++ RandomPassword.py
@@ -1,8 +1,18 @@
 import random
 
-characters = "abcdefghijklmnopqrstvxyzABCDEFGHIJKLMNOPQRSTVXZ"
+characters = "abcdefghijklmnopqrstvxyzABCDEFGHIJKLMNOPQRSTVXZ!@#$%^&*()_+}:"
 status = True
-
+counter = 0
 while status:
   password_length = int(input("What length do you want your password to be? "))
   password_count = int(input("How many password do you want? "))
+  for i in range(password_count):
+    password = " "
+    for j in range(password_length):
+      random_character = random.choice(characters)
+      password += random_character
+
+    print("Here is your password : " + password)
+    counter += 1
+    if counter == password_count:
+      status = False
