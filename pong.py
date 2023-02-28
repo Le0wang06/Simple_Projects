48ace14ca70767b8c5eb0376e3a1a98408311bdc
Leo Wang <77943019+Le0wang06@users.noreply.github.com>
2023-02-28 02:12:03 +0000

    commit

--- pong.py
+++ pong.py
@@ -1,4 +1,5 @@
-#Credits: freeCodeCamp.org
+# Credits: freeCodeCamp.org
+#https://www.youtube.com/watch?v=XGf2GcyHPhc
 
 import turtle
 
@@ -10,12 +11,10 @@ win.tracer(0)
 
 #paddle one
 paddle_1 = turtle.Turtle()
-paddle_1.tracer(0)
+paddle_1.speed(0)
 paddle_1.shape("square")
 paddle_1.color("white")
-
 paddle_1.shapesize(stretch_wid=5,stretch_len=1)
-
 paddle_1.penup()
 paddle_1.goto(-350,0)
 
